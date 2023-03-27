6e458a7d83d340d836aa6f78e9b1605c0d82b3c5
cmy_desk <devcmy136@gmail.com>
2023-03-27 16:37:19 +0900

    blog/blogcomment수정

--- src/main/java/com/itwill/ilhajob/blogComment/BlogComment.java
+++ src/main/java/com/itwill/ilhajob/blogComment/BlogComment.java
@@ -1,5 +1,32 @@
 package com.itwill.ilhajob.blogComment;
 
-public class BlogComment {
+import java.util.Date;
 
+import com.itwill.ilhajob.blog.Blog;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ * 
+CREATE TABLE blog_comment(
+		comment_seq                   		NUMBER(20)		 NULL ,
+		comment_content               		VARCHAR2(50)		 NULL ,
+		comment_date                  		DATE		 NULL ,
+		user_email                    		VARCHAR2(50)		 NULL ,
+		blog_seq                      		NUMBER(20)		 NOT NULL
+);
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BlogComment {
+	private int commentSeq;
+	private String commentContent;
+	private Date commentDate;
+	private String userEmail;
+	private int blogSeq;
 }
