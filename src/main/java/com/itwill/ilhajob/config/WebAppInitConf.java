87c74d6ef984ea7c7dd9052deacab9f45bd2929c
chosd <racoCSD@gmail.com>
2023-03-27 14:00:16 +0900

    ..

--- src/main/java/com/itwill/ilhajob/config/WebAppInitConf.java
+++ src/main/java/com/itwill/ilhajob/config/WebAppInitConf.java
@@ -15,32 +15,12 @@ import org.springframework.web.servlet.DispatcherServlet;
  * -  톰캣실행시 기본설정
  */
 public class WebAppInitConf implements WebApplicationInitializer {
+
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
+		// TODO Auto-generated method stub
 		
-				AnnotationConfigWebApplicationContext context = 
-						new AnnotationConfigWebApplicationContext();
-				context.register(WebConfig.class);
-				ServletRegistration.Dynamic dynamic=
-						servletContext.addServlet("dispatcherSevlet", new DispatcherServlet(context));
-				dynamic.setLoadOnStartup(0);
-				dynamic.addMapping("/");
-				/*
-				//ApplicationConfig.java : config 설정 클래스를 지정해주는 것
-				AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
-				applicationContext.register(ApplicationConfig.class);
-				//Listener
-				servletContext.addListener(new ContextLoaderListener(applicationContext));
-				//WebConfig.java
-				AnnotationConfigWebApplicationContext servletConfig = new AnnotationConfigWebApplicationContext();
-				servletConfig.register(WebConfig.class);
-				ServletRegistration.Dynamic dispatcherServlet = servletContext.addServlet("dispatcherServlet", new DispatcherServlet(servletConfig));
-				dispatcherServlet.setLoadOnStartup(1);
-				dispatcherServlet.addMapping("/");
-				//encodingFilter
-				FilterRegistration.Dynamic filterRegistration = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter("UTF-8", true));
-				filterRegistration.addMappingForUrlPatterns(null, true, "/*");
-				*/
 	}
+	
 	
 }
