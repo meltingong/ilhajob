221d5ddf7be9713ddaea10ef610d637ab22c20e4
ys9890lggdd@naver.com <ys9890lggdd@naver.com>
2023-03-27 18:50:29 +0900

    App Mapper 작업

--- src/main/java/com/itwill/ilhajob/app/App.java
+++ src/main/java/com/itwill/ilhajob/app/App.java
@@ -1,5 +1,22 @@
 package com.itwill.ilhajob.app;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ *  사용자관리를 위하여 필요한 도메인클래스(VO,DTO)
+ *  USERINFO 테이블의 각컬럼에해당하는 멤버를 가지고있다
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class App {
+	
+	private String appSeq;
+	private String appStatus;
+	private String rcSeq;
+	private String cvSeq;
 
 }
