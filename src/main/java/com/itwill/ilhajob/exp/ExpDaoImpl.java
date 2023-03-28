f97f7cc7b7dc34ee04fba3b4c7e0f02c0b7520cd
meltingong <hwk1210@naver.com>
2023-03-28 16:01:02 +0900

    ..

--- src/main/java/com/itwill/ilhajob/exp/ExpDaoImpl.java
+++ src/main/java/com/itwill/ilhajob/exp/ExpDaoImpl.java
@@ -1,5 +1,7 @@
 package com.itwill.ilhajob.exp;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -27,7 +29,34 @@ public class ExpDaoImpl {
 	}
 	
 	/*
+	 * 경력 전체보기 (admin 없어서 필요없을 것 같음)
+	 
+	 public List<Exp> selectAll(){
+		 List<Exp> expList = expMapper.selectAll();
+		 return expList;
+	 }
+	*/
+
+	/*
+	 * 경력 seq로 경력사항 찾기(필요없을 듯)
+	 * 
+		public Exp selectByExpSeq(int expSeq) {
+			Exp findExp = expMapper.selectByExp(expSeq);
+			return findExp;
+		}
 	 * 
 	 */
 	
+	
+	 /*
+	  * 한 유저의 경력 전체보기(userSeq로 찾기)
+	  */
+	 
+	 public List<Exp> selectByUser(int userSeq){
+		 List<Exp> expList = expMapper.selectByUserSeq(userSeq);
+		 return expList;
+	 }
+	
+	 
+	 
 }
