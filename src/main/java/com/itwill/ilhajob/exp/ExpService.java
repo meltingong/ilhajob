ebc11ae4467e44c0f1b7f675c41ce7331d514d16
meltingong <hwk1210@naver.com>
2023-03-28 16:34:45 +0900

    ..

--- src/main/java/com/itwill/ilhajob/exp/ExpService.java
+++ src/main/java/com/itwill/ilhajob/exp/ExpService.java
@@ -5,18 +5,22 @@ import java.util.List;
 public interface ExpService {
 	
 	/*
-	 * 경력사항 추가
-	 */
-	int insertExp(Exp exp);
-	
-	/*
 	 * 유저seq로 경력사항 찾기
 	 */
 	
 	List<Exp> selectByUserSeq(int userSeq);
 	
 	/*
-	 * 
+	 * 경력사항 추가
 	 */
+	int insertExp(Exp exp);
 	
+	/*
+	 * 경력 수정
+	 */
+	int updateExp(Exp exp);
+	/*
+	 * 경력 삭제
+	 */
+	int deleteExp(int expSeq);
 }
