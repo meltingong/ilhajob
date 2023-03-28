cc9b183abe111ebe2bc6d466aec47274f86f000c
meltingong <hwk1210@naver.com>
2023-03-28 16:33:46 +0900

    ..

--- src/main/java/com/itwill/ilhajob/exp/ExpService.java
+++ src/main/java/com/itwill/ilhajob/exp/ExpService.java
@@ -1,8 +1,22 @@
 package com.itwill.ilhajob.exp;
 
+import java.util.List;
+
 public interface ExpService {
 	
+	/*
+	 * 경력사항 추가
+	 */
+	int insertExp(Exp exp);
 	
+	/*
+	 * 유저seq로 경력사항 찾기
+	 */
 	
+	List<Exp> selectByUserSeq(int userSeq);
+	
+	/*
+	 * 
+	 */
 	
 }
