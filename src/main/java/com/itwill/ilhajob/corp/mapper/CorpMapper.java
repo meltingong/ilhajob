445f493016701b34b62d03f0170464ac66a32288
kangcarry <gangsangim147@gmail.com>
2023-03-28 17:33:25 +0900

    .

--- src/main/java/com/itwill/ilhajob/corp/mapper/CorpMapper.java
+++ src/main/java/com/itwill/ilhajob/corp/mapper/CorpMapper.java
@@ -2,11 +2,8 @@ package com.itwill.ilhajob.corp.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Mapper;
-
 import com.itwill.ilhajob.corp.Corp;
 
-@Mapper
 public interface CorpMapper {
 	/*
 	 * 메쏘드이름은 	GuestMapper.xml 파일의 id와일치
@@ -15,7 +12,7 @@ public interface CorpMapper {
 	 */
 	public Corp selectById(String corp_id);
 	public List<Corp> selectAll();
-	public int insertGuest(Corp corp);
-	public int deleteGuest(String corp_id);
-	public int updateGuest(Corp corp);
+	public int insertCorp(Corp corp);
+	public int deleteCorp(String corp_id);
+	public int updateCorp(Corp corp);
 }
--- src/main/java/com/itwill/ilhajob/corpimage/mapper/CorpImageMapper.java
+++ src/main/java/com/itwill/ilhajob/corpimage/mapper/CorpImageMapper.java
@@ -1,5 +1,16 @@
 package com.itwill.ilhajob.corpimage.mapper;
 
-public interface CorpImageMapper {
+import java.util.List;
 
+import com.itwill.ilhajob.corp.Corp;
+import com.itwill.ilhajob.corpimage.CorpImage;
+
+public interface CorpImageMapper {
+	public int insertCorpImage(CorpImage corp_image);
+	public CorpImage selectBySEQ(int corp_image_seq);
+	public List<CorpImage> selectById(String corp_id);
+	public List<CorpImage> selectAll();
+	public int updateCorpImage(CorpImage corp_image);
+	public int deleteCorpImageById(String corp_id);
+	public int deleteCorpImageBySEQ(int corp_image_seq);
 }
