dfbc64d2f2f92106d227d271ede844365b20c681
meltingong <hwk1210@naver.com>
2023-03-28 15:31:38 +0900

    ..

--- src/main/java/com/itwill/ilhajob/exp/mapper/ExpMapper.java
+++ src/main/java/com/itwill/ilhajob/exp/mapper/ExpMapper.java
@@ -1,6 +1,20 @@
 package com.itwill.ilhajob.exp.mapper;
 
-public class ExpMapper {
-	
+import java.util.List;
 
+import com.itwill.ilhajob.exp.Exp;
+
+public interface ExpMapper {
+
+	public Exp selectByExp(int expSeq);
+	
+	public List<Exp> selectAll();
+	
+	public List<Exp> selectByUserSeq(int userSeq);
+	
+	public int insertExp(Exp exp);
+	
+	public int updateExp(Exp exp);
+	
+	public int deleteExp(int expSeq);
 }
