bca496f90e4e0cf0c5494ebaae994ed53fd9734f
meltingong <hwk1210@naver.com>
2023-03-28 15:50:52 +0900

    ,,

--- src/main/java/com/itwill/ilhajob/exp/ExpDaoImpl.java
+++ src/main/java/com/itwill/ilhajob/exp/ExpDaoImpl.java
@@ -1,9 +1,22 @@
 package com.itwill.ilhajob.exp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.itwill.ilhajob.exp.mapper.ExpMapper;
+
+@Repository
 public class ExpDaoImpl {
-
-	public static void main(String[] args) {
-
+	@Autowired
+	private ExpMapper expMapper;
+	
+	public ExpDaoImpl(ExpMapper expMapper) {
+		this.expMapper = expMapper;
 	}
 
+	public ExpDaoImpl() {
+
+	}
+	
+	
 }
