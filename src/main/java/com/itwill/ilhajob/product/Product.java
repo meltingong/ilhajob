34b25a09dbe93fcfb67d30bd918560a0339c95c3
songdh97 <songdh97@gmail.com>
2023-03-28 16:31:18 +0900

    product

--- src/main/java/com/itwill/ilhajob/product/Product.java
+++ src/main/java/com/itwill/ilhajob/product/Product.java
@@ -1,5 +1,19 @@
 package com.itwill.ilhajob.product;
 
-public class Product {
+import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+	private int p_no;
+	private String p_name;
+	private int p_price;
+	private Date p_date;
+	private String p_image;
+	private String p_div;
 }
