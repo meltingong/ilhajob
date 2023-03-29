ab229e428541dd3e684b17c3511c264723ff5768
minyoung.c_itwill <devcmy136@gmail.com>
2023-03-29 15:46:54 +0900

    awards controller 틀 작성

--- src/main/java/com/itwill/ilhajob/awards/AwardsController.java
+++ src/main/java/com/itwill/ilhajob/awards/AwardsController.java
@@ -1,7 +1,15 @@
 package com.itwill.ilhajob.awards;
 
-public class AwardsController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
+//@RestController
+//@Controller
+public class AwardsController {
+	
+	@Autowired
+	private AwardsService awardsService;
 	
 
 }
