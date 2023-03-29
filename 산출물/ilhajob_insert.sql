ac4f9241411feb07a25148e245526845a6376176
kangcarry <gangsangim147@gmail.com>
2023-03-29 15:32:13 +0900

    corp,insert변경

--- 산출물/ilhajob_insert.sql
+++ 산출물/ilhajob_insert.sql
@@ -27,120 +27,120 @@ insert into corp(corp_id, corp_password, corp_name) values('기업6@corp.com','6
 
 
 insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('corp_01', '1111', '신세계아이앤씨', '02-3397-1234', '111-11-11111', 'http://shinsegae-inc.com', '1997-02-10', '1,339명', '58억 8100만원',
 '신세계아이앤씨는 디지털 기술로 새로운 영역에서 새로운 가치를 만들어내는 글로벌 리테일테크 전문기업입니다.', '연금,보험,휴무 상의 후 결정', '서울 중구 남대문시장10길 2 (회현동1가, MESA) 21층', 1, null, null );
 
 insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('corp_02', '2222', '우아한청년들', '070-4352-5581', '222-22-22222', ' http://career.woowayouths.com', '2015-11-10', '250명', '28억 7700만원',
 '새로운 배달 인프라, 당신과 우아한청년들이 함께합니다.', '연금,보험,휴무 상의 후 결정', '서울 송파구 올림픽로35다길 32 (신천동) , 9층(예전빌딩)',  1, null, null);
 
 insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('corp_03', '3333', '메타넷디지털(주)', '02-3704-5114', '333-33-33333', 'http://metanetglobal.com', '1989-01-21', '1200명', '5억7000만원',
 '메타넷디지털은 국내 IT산업의 선두반열에서 첨단의 정보기술을 개발해온 종합정보 서비스 회사입니다.','연금,보험,휴무 상의 후 결정', '서울 종로구 종로33길 15 연강빌딩', 1, null,  null);
 
 insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('corp_04', '4444', '제이엘케이', '02-6925-6189', '444-44-44444', 'http://jikgroup.com', '2014-12-15', '80명', '40억 2000만원',
 '제이엘케이는 의료진단 분야, 원격의료분야, 데이터분야에서 세계를 선도하는 인공지능 플랫폼 기업입니다.', '연금,보험,휴무 상의 후 결정', '서울 강남구 테헤란로33길 5 (역삼동, JLK타워) JLK타워', 1,null,  null);
 
 insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('corp_05', '5555', '에이치엠엠', '02-3706-5114', '555-55-55555', 'http://hmm21.com','1976-09-19', '1,785명', '18억 4000만원',
 '반드시 승리하고 미래를 창조하는 현대상선 그룹입니다.', '연금,보험,휴무 상의 후 결정', '서울 종로구 연지동 1-7 현대그룹빌딩 현대상선', 1,null, null);
 
 insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('corp_06', '6666', '랜드소프트(주)', '070-8864-4777', '666-66-66666', 'http://landsoft.co.kr', '2018-05-09', '50명', '15억 2000만원',
 '랜드소프트 주식공 정보시스템 기업으로 2018년 국내 전문가들이 모여 시작해 매년 매출액이 200%이상 성장하고 있는 강소기업입니다.', '연금,보험,휴무 상의 후 결정', '서울 서초구 서래로 37 (반포동) 2,3층', 1, null, null);
 
 
 insert into corp(CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('DBtest1','1111','더문랩스','02-1111-1111','123-456-7890','http://www.DBtest1.com','2021-01-01','45','1000000000','DB회사입니다.1','칼퇴','서울시 강남구','1',null,null);
 insert into corp(CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('DBtest2','2222','마켓링크','02-2222-2222','123-456-7899','http://www.DBtest2.com','2020-01-02','20','2000000000','DB회사입니다.2','칼퇴','서울시 서초구','1',null,null);
 insert into corp(CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('DBtest3','3333','디비에프아이에스','02-3333-3333','123-456-7898','http://www.DBtest3.com','2020-01-03','30','3000000000','DB회사입니다.3','칼퇴','서울시 중랑구','1',null,null);
 insert into corp(CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('DBtest4','4444','대보정보통신','02-4444-4444','123-456-7897','http://www.DBtest4.com','2020-01-04','40','4000000000','DB회사입니다.4','칼퇴','서울시 도봉구','1',null,null);
 insert into corp(CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('DBtest5','5555','오앤이데이타','02-5555-5555','123-456-7896','http://www.DBtest5.com','2020-01-05','50','5000000000','DB회사입니다.5','칼퇴','서울시 광진구','1',null,null);
 insert into corp(CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('DBtest6','6666','앱노리','02-6666-6666','123-456-7895','http://www.DBtest6.com','2020-01-06','60','6000000000','DB회사입니다.6','칼퇴','서울시 송파구','1',null,null);
 
-insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID)  
+insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE)  
 values('corp_13', '1313', '다큐브(daquv)', '02-456-7891', '763-87-02018', 'http://daquv.com', '2021-01-19', '20', '0',
 '다큐브는 국내 최초 기업 맞춤형 음성비서 서비스를 제공하는 B2B 전문기업 입니다. ', '연금,보험,휴무,보상,주거비지원','서울시 영등포구 여의나루로 67 신송빌딩 12 층', '1', null, null );
 
-insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE, CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID)  
+insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE, CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE)  
 values('corp_14', '1414', '(주)다이얼커뮤니케이션즈', '1577-7530', '206-81-84553', ' http://www.maaltalk.com/', '2003-09-06', '18', '12408000000',
 '(주)다이얼커뮤니케이션즈는 MaalTalk, VoIP Solutions, 화상회의솔루션 의 기술을 바탕으로 최적의 통합 커뮤니케이션 서비스를 제공하는 사업을 하고 있습니다.', '연금,보험,휴무,보상', '서울 강서구 공항대로 168 (마곡동) 마곡 747 타워 411~413 ', '1', null, null);
 
-insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE, CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE, CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('corp_15', '1515', '(주) 테이블링', '02-1899-9195', '842-86-00373', 'http://www.tabling.co.kr/', '2016-04-14', '78', '456300000000',
 '테이블링은 스마트하게 예약과 대기가 가능한 서비스로 디지털 트렌스포메이션으로 탐색에서
 부터 식사 이후 까지의 모든 과정에서의 낭비가 없어진 세상 이라는 비전으로 전진 하고 있습
 니다.','연금,보험,휴무,보상,휴가',  '서울 강남구 영동대로 502 (삼성동) 위메프',  '1', null, null );
 
-insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE, CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE, CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('corp_16', '1616', '무신사', '1544-7199', '211-88-79575', 'https://www.musinsa.com', '2012-06-25', '1329', '402400000000',
 '무신사는 700 만 회원을 보유한 국내 1 위 온라인 패션 플랫폼입니다. 스트릿, 글로벌 명품, 디
 자이너 등 5 천여 개 브랜드가 입점한 「무신사 스토어」와 국내·외 최신 패션 트렌드와 정보를
 전달하는 패션 매거진 「무신사 매거진」을 운영하고 있습니다.', '연금,보험,휴무,보상,포인트지급', '서울 성동구 아차산로 13 길 11, 1 층',  '1', null, null );
 
-insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE, CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE, CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('corp_17', '1717', '하이브', '02-3444-0105', '120-86-78223', 'https://www.hybecorp.com','2005-02-04', '633', '402440650000',
 '하이브(HYBE)는 “We believe in music”이라는 미션 아래 음악 산업의 비즈니스 모델을 혁신하
 는 기업입니다. 하이브는 음악에 기반한 세계 최고의 엔터테인먼트 라이프스타일 플랫폼 기업을
 지향합니다.',
 '연금,보험,휴무,보상,자율출퇴근', '서울시 용산구 한강대로 42',  '1', null, null );
 
-insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE, CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID) 
+insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE, CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE) 
 values('corp_18', '1818', '슈피겐코리아(Spigen)', '02-862-3452', '120-87-36593', 'http://www.spigen.co.kr', '2009-02-18', '386', '265970000000',
 '슈피겐코리아는 모바일 액세서리 및 소형 전자기기를 제조하여 전 세계에 팔고 있는 제조·유
 통 기업입니다.',
 '연금,보험,휴무,보상,원격근무', '서울특별시 강남구 봉은사로 446 슈피겐 HQ A 동', '1', null, null);
 
 insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID)
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE)
 values('corp_19', '1212', '한글과컴퓨터', '1566-5192', '111-222-3333', 'http://www.hancom.com', '1990-10-11', '434명', '1350억',
 '한글과컴퓨터는 새로워진 한컴디벨로퍼를 통해 한컴의 기술을 제공하고 문서편집까지 가능한 올인원 전자서명 서비스를 제공하고 있습니다.',
 '연금,보험,휴무,보상', '경기 성남시 분당구 대왕판교로644번길 49 (삼평동, 코리아벤처타운) 한컴타워10층', 1, null, null);
 
 insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID)
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE)
 values('corp_20', '2323', '더존비즈온', '02-6233-3000', '112-222-3333', 'http://www.douzone.com', '1977-08-20', '1675명', '3031억',
 '기업, 금융, 커머스, 헬스케어, 공공 분야 등에서 Work''&''Life 토털 솔루션을 제공하는 대한민국 대표 ICT그룹입니다.기업정보화 선도기업인 더존은 정보화에 필요한 각종 Solution과 Service를 제공합니다.',
 '연금,보험,휴무,보상', '강원 춘천시 남산면 수동리 749번지 더존IT그룹 강촌캠퍼스', 1, null, null);
 
 insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID)
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE)
 values('corp_21', '3434', '비즈테크아이', '02-2084-6700', '113-333-3333', 'http://www.biztechpartners.co.kr', '2003-01-09', '765명', '1492억8천',
 '비즈테크아이는 Global IT 서비스 전문기업인 LG CNS 자회사로 기술전문성을 가지고 SAP ERP와 Web Service 분야에서 차별화된 서비스를 제공합니다.​',
 '연금,보험,휴무,보상', '서울 마포구 매봉산로 75 (상암동, 디디엠씨(DDMC)) 12층', 1, null, null);
 
 insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID)
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE)
 values('corp_22', '4545', '코매퍼', '02-565-6713', '114-444-3333', 'http://ko-mapper.com', '2019-01-21', '13명', '3억8천',
 '코매퍼는 실제 현장 적용이 가능한 서비스를 제공하는 세계 최고수준의 3D Mapping/Modeling 솔루션 기업​입니다.',
 '연금,보험,휴무,보상', '서울 강남구 봉은사로57길 13 (삼성동, 프러스원) 2층', 1, null, null);
 
 insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID)
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE)
 values('corp_23', '5656', '소프트웍스', '02-6401-8937', '115-555-3333', 'http://www.softworks.co.kr', '2011-05-20', '80명', '5억4천4백',
 '소프트웍스는 전사 비즈니스에 대한 정보자산의 가치, 위협과 취약점 관리 능력을 강화시켜 Compliance Risk 를 최소화 할 수 있도록 지원하는 기업입니다.',
 '연금,보험,휴무,보상', '서울 서초구 사임당로10길 3 (서초동, 원진빌딩) 501호', 1, null, null);
 
 insert into corp (CORP_ID,CORP_PASSWORD,CORP_NAME,CORP_PHONE,CORP_BUSINESS_NO,CORP_WEBSITE,
-CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE_ID)
+CORP_EST,CORP_SIZE,CORP_SALES,CORP_COMMENT,CORP_WELFARE,CORP_ADDRESS,CORP_STATUS,JOB,ROLE)
 values('corp_24', '6767', '온넷시스템즈코리아', '02-2057-2260', '116-666-3333', 'http://www.onnetsystemskorea.com', '2001-02-09', '34명', '29억6천',
 '온넷시스템즈코리아는 콘텐츠 전송속도의 저하를 막고 불안정성 문제를 해결하기 위해 고안된 기술로, ISP(internet service provider)의 네트워크 하단에 여러 대의 캐시서버를 설치,CP(Contents Provider)가 제공하는 콘텐츠를 이 캐시서버에 미리 옮겨놓고 수요가 있을 때 그 콘텐츠를 사용자에게 제공하는 기업입니다.',
 '연금,보험,휴무,보상', '서울 서초구 효령로 262 (서초동) 평화빌딩 4층', 1, null, null);
