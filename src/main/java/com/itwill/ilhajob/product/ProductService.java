807c19e7c59206ed3856cca0a2fc77ae5cc573c7
송도현 <@DESKTOP-PQA2U6C>
2023-03-29 13:56:23 +0900

    product service

--- src/main/java/com/itwill/ilhajob/product/ProductService.java
+++ src/main/java/com/itwill/ilhajob/product/ProductService.java
@@ -1,5 +1,19 @@
 package com.itwill.ilhajob.product;
 
-public interface ProductService {
+import java.util.List;
 
+public interface ProductService {
+	
+	//상품 번호로 상품 1개 찾기
+	Product selectByNo(int pNo) throws Exception;
+	
+	//p_div로 상품 리스트 불러오기
+	List<Product> selectAllByDiv(String pDiv) throws Exception;
+	
+	//update Product
+	int updateProduct(Product product) throws Exception;
+	
+	//delete Product
+	int deleteProduct(int pNo) throws Exception;
+	
 }
