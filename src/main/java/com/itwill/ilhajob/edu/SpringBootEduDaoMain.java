c52e00e13b7ead9db2b9216c138827d852a77828
breeeeeeeeze <earth9563@gmail.com>
2023-03-29 18:21:45 +0900

    ..

--- src/main/java/com/itwill/ilhajob/edu/SpringBootEduDaoMain.java
+++ src/main/java/com/itwill/ilhajob/edu/SpringBootEduDaoMain.java
@@ -14,14 +14,18 @@ public class SpringBootEduDaoMain {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(SpringBootEduDaoMain.class, args);
 		EduDao eduDao = (EduDao)applicationContext.getBean(EduDao.class);
-		System.out.println("------ EduDaoTest-----");
-		System.out.println(eduDao.insertEdu(new Edu(0, "영문학과", "서울대학교", new Date(), new Date(), 3.45, "학력 설명", 3)));
-		System.out.println(eduDao.selectAllEdu());
-		System.out.println(eduDao.selectEduByUserSeq(3));
-		Edu updateEdu = new Edu(9, "수학과", "고려대학교", new Date(), new Date(), 4.1, "수학과 졸업", 2);
-		System.out.println(eduDao.updateEdu(updateEdu));
-		System.out.println(eduDao.deleteEduByEduSeq(8));
-		System.out.println(eduDao.deleteAllEdu(3));
+//		System.out.println("------ EduDaoTest-----");
+//		System.out.println(eduDao.insertEdu(new Edu(0, "영문학과", "서울대학교", new Date(), new Date(), 3.45, "학력 설명", 3)));
+//		System.out.println(eduDao.selectAllEdu());
+//		System.out.println(eduDao.selectEduByUserSeq(3));
+//		Edu updateEdu = new Edu(9, "수학과", "고려대학교", new Date(), new Date(), 4.1, "수학과 졸업", 2);
+//		System.out.println(eduDao.updateEdu(updateEdu));
+//		System.out.println(eduDao.deleteEduByEduSeq(8));
+//		System.out.println(eduDao.deleteAllEdu(3));
+		
+		EduService eduService = (EduService)applicationContext.getBean(EduService.class);
+		System.out.println("------ EduServiceTest-----");
+		System.out.println(eduService.insertEdu(new Edu(0, "사회학과", "성균관대학교", new Date(), new Date(), 3.6, "", 2)));
 	}
 
 }
