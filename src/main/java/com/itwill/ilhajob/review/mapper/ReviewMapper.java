77ae96deb98c602eec4e92f2771159ae62b542d8
songdh97 <songdh97@gmail.com>
2023-03-30 17:58:34 +0900

    Review Mapper 1차

--- src/main/java/com/itwill/ilhajob/review/mapper/ReviewMapper.java
+++ src/main/java/com/itwill/ilhajob/review/mapper/ReviewMapper.java
@@ -1,5 +1,28 @@
 package com.itwill.ilhajob.review.mapper;
 
-public interface ReviewMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.itwill.ilhajob.review.Review;
+
+@Mapper
+public interface ReviewMapper {
+	
+	//insert review
+	public int insertReview(Review review) throws Exception;
+	
+	//update review
+	public int updateReview(Review review) throws Exception;
+	
+	//user_seq 사용해서 delete review
+	public int deleteByUserSeq(int userSeq) throws Exception;
+	
+	//user_seq로 user가 쓴 리뷰 목록 불러오기
+	public List<Review> selectReivewListbyUserSeq(int userSeq) throws Exception;
+	
+	//corp_id로 해당 기업의 리뷰 목록 불러오기
+	public List<Review> selectReviewListByCorpId(String corpId) throws Exception;
+	
+	//user 1명이 기업 1에 리뷰 중복 작성 안됨: 중복 체크
 }
