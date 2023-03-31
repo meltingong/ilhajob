ff5c82ccb4ea5ab781142e45e59fcb6809bddbf4
jihyun <jihyunyeah@naver.com>
2023-03-31 14:42:06 +0900

    ..

--- src/main/java/com/itwill/ilhajob/blog/SpringBootBlogDaoMain.java
+++ src/main/java/com/itwill/ilhajob/blog/SpringBootBlogDaoMain.java
@@ -27,13 +27,13 @@ public class SpringBootBlogDaoMain {
 		//System.out.println(blogDao.selectByBlogCate(1));
 		
 		//System.out.println("--------insertBlog test--------");
-		//System.out.println(blogDao.insertBlog(new Blog(4, "TEST1","내용1", "이미지1", new Date(),4,4,"4",4)));
+		//System.out.println(blogDao.insertBlog(new Blog(4, "TEST1","내용1", "이미지1", new Date(),1,2,"3",3)));
 		
 		//System.out.println("--------updateBlog test--------");
 		//System.out.println(blogDao.updateBlog(new Blog(60, "제목수정", "내용수정", "이미지수정", new Date(),3,3,"3",3)));
 		
 		//System.out.println("--------deleteBlog test--------");
-		//System.out.println(blogDao.deleteBlog(60));
+		//System.out.println(blogDao.deleteBlog(61));
 		
 	}
 }
