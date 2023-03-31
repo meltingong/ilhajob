fa756e66044a2df1be12ec81aaca1df6970cfce7
ys9890lggdd@naver.com <ys9890lggdd@naver.com>
2023-03-31 16:30:06 +0900

    blog comment dao 테스트 수정

--- src/main/java/com/itwill/ilhajob/blogComment/SpringBootBlogCommentDaoMain.java
+++ src/main/java/com/itwill/ilhajob/blogComment/SpringBootBlogCommentDaoMain.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.itwill.ilhajob.blog.Blog;
+
 @SpringBootApplication
 @MapperScan(basePackages = "com.itwill.ilhajob.blogComment.mapper")
 //@MapperScan(basePackages = "com.itiwll.ilhajob.Awards.mapper")
@@ -18,15 +20,27 @@ public class SpringBootBlogCommentDaoMain {
         //Awards tutorDao=(Awards)appicationContext.getBean(Awards.class);		
 	    BlogCommentDao blogCommentDao=(BlogCommentDao)appicationContext.getBean(BlogCommentDao.class);
 	    //System.out.println("--------findBlogComment--------");
-	    //System.out.println(blogCommentDao.findBlogComment(91));    
-		//System.out.println("--------selectAll--------");
-		//System.out.println(blogCommentDao.selectAll());
+	    //System.out.println(blogCommentDao.findBlogComment(6));    
+		System.out.println("--------selectAll--------");
+		System.out.println(blogCommentDao.selectAll());
 	    //System.out.println("--------selectByBlogComment--------");
-	    //System.out.println(blogCommentDao.selectByBlogComment(56));
-	    //System.out.println("--------insertBlogComment--------");
-	    //System.out.println(blogCommentDao.insertBlogComment(new BlogComment(95, "블로그 댓글내용5", new Date(),"1",59)));
-	    //System.out.println("--------updateBlogComment--------");
-	    //System.out.println(blogCommentDao.updateBlogComment(new BlogComment(95, "블로그 댓글내용수정",new Date(),"1",59)));
+	    //System.out.println(blogCommentDao.selectByBlogComment(1));
+	    //System.out.println("--------insertBlogComment test--------");
+		//System.out.println(blogCommentDao.insertBlogComment(BlogComment.builder()
+		//											.commentSeq(8)
+		//											.commentContent("내용1")
+		//											.commentDate(new Date())																	
+		//											.userSeq(1)			
+		//											.blogSeq(1)
+		//											.build()));	    		                                                           		                                                        
+	    //System.out.println("--------updateBlogComment test--------");
+		//System.out.println(blogCommentDao.insertBlogComment(BlogComment.builder()
+		//											.commentSeq(8)
+		//											.commentContent("내용수정")
+		//											.commentDate(new Date())																	
+		//											.userSeq(1)			
+		//											.blogSeq(2)
+		//											.build()));
 	    //System.out.println("--------deleteBlogComment--------");
 	    //System.out.println(blogCommentDao.deleteBlogComment(95));
 	
