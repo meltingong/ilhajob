bb7f1d019bea4066d103a3af93c9042633a8bb66
minyoung.c <devcmy136@gmail.com>
2023-03-31 22:26:36 +0900

    controller annotation 작성

--- src/main/java/com/itwill/ilhajob/message/MessageController.java
+++ src/main/java/com/itwill/ilhajob/message/MessageController.java
@@ -1,5 +1,13 @@
 package com.itwill.ilhajob.message;
 
-public class MessageController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
+//@RestController
+//@Controller
+public class MessageController {
+	
+	@Autowired
+	private MessageService messageService;
 }
