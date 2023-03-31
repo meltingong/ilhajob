3064d8b8cbf3add8cc2a9c20037ae08dcf33f6a6
chosd <racoCSD@gmail.com>
2023-03-31 17:48:51 +0900

    ..

--- src/main/java/com/itwill/ilhajob/corp/CorpServiceImpl.java
+++ src/main/java/com/itwill/ilhajob/corp/CorpServiceImpl.java
@@ -3,11 +3,42 @@ package com.itwill.ilhajob.corp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.itwill.ilhajob.user.User;
+
 @Service
 public class CorpServiceImpl implements CorpService{
 	
 	@Autowired
 	private CorpDao corpDao;
 	
-	
+	@Override
+	public int create(User user) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int login(Corp corp) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public User findUser(String corpId) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public int update(Corp corp) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int remove(String corpId) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public boolean isDuplicateId(String corpId) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
