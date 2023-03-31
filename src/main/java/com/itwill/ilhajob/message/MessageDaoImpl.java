6c80e464db3df4590c6034e0e94a7385b0afad8d
minyoung.c <devcmy136@gmail.com>
2023-03-31 20:57:25 +0900

    message dto,dao 작성중~_~

--- src/main/java/com/itwill/ilhajob/message/MessageDaoImpl.java
+++ src/main/java/com/itwill/ilhajob/message/MessageDaoImpl.java
@@ -1,5 +1,57 @@
 package com.itwill.ilhajob.message;
 
-public class MessageDaoImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.itwill.ilhajob.message.mapper.MessageMapper;
+
+@Repository
+public class MessageDaoImpl implements MessageDao{
+	@Autowired
+	private MessageMapper messageMapper;
+
+	@Override
+	public Message findMessage(int messageSeq) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int createMessage(Message message) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateMessage(Message message) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int removeMessageBySeq(int messageSeq) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int removeMessageByUserSeq(int userSeq) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Message> findMessageList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Message> findMessageOfUser(int userSeq) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
