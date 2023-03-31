4032b812204106d88d9fa0fde208f86e3394444e
kangcarry <gangsangim147@gmail.com>
2023-03-31 18:02:52 +0900

    corpTag dao test완료

--- src/main/java/com/itwill/ilhajob/corpTag/CorpTagDaoImpl.java
+++ src/main/java/com/itwill/ilhajob/corpTag/CorpTagDaoImpl.java
@@ -1,5 +1,38 @@
 package com.itwill.ilhajob.corpTag;
 
-public class CorpTagDaoImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.itwill.ilhajob.corpTag.mapper.CorpTagMapper;
+@Repository
+public class CorpTagDaoImpl implements CorpTagDao{
+	@Autowired
+	CorpTagMapper corpTagMapper;
+	
+	@Override
+	public int insertCorpTag(CorpTag corpTag) {
+		return corpTagMapper.insertCorpTag(corpTag);
+	}
+
+	@Override
+	public List<String> selectCorpByTag(int tagId) {
+		return corpTagMapper.selectCorpByTag(tagId);
+	}
+
+	@Override
+	public int deleteCorpTag(CorpTag corpTag) {
+		return corpTagMapper.deleteCorpTag(corpTag);
+	}
+
+	@Override
+	public boolean existedCorpTag(String corpId,int tagId) {
+		if(corpTagMapper.existedCorpTag(CorpTag.builder().corpId(corpId).tagId(tagId).build())==1){
+			return true;
+		}else {
+			return false;
+		}
+	}
 
 }
--- src/main/java/com/itwill/ilhajob/recruitTag/mapper/RecruitTagMapper.java
+++ src/main/java/com/itwill/ilhajob/recruitTag/mapper/RecruitTagMapper.java
@@ -1,5 +1,8 @@
 package com.itwill.ilhajob.recruitTag.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface RecruitTagMapper {
 
 }
