a28e1ceb997b0de93702d0e635ad59ce668f75db
meltingong <hwk1210@naver.com>
2023-04-04 10:19:49 +0900

    ..

--- src/main/java/com/itwill/ilhajob/user/UserController.java
+++ src/main/java/com/itwill/ilhajob/user/UserController.java
@@ -34,5 +34,36 @@ public class UserController {
 	public String user_excetpion_handler(Exception e) {
 		return "user_error";
 	}
-
+	
+	/*
+	 * <<유저 정보>> my profile 유저정보 업데이트 폼
+	 * 회원 로그인
+	 * 회원 로그아웃
+	 * 회원 정보 수정
+	 * 회원 탈퇴
+	 * 회원 정보 보기
+	 */
+	/*
+	 * dashboard-resume-alerts.html
+	 * <<유저 알림>>
+	 * 지원 완료 알림 , 지원 상태(서류합격) 알림 , 이력서 열람 알림
+	 */
+	
+	// my resume 이력서 작성 폼
+	
+	/*
+	 * <<지원>> applied jobs 지원한 공고 목록
+	 * 회사에 이력서 지원 하기
+	 * 이력서 지원한 회사 목록 보기
+	 */
+	
+	/*
+	 * <<오더목록>> Packages 개인회원이 결재한 목록
+	 * 주문한 목록 보기
+	 */
+	
+	/*
+	 * 리뷰(기업에 대한 리뷰) -- > 템플릿 페이지 만들 필요 있음
+	 * 리뷰 작성 , 삭제 , 수정 
+	 */
 }
