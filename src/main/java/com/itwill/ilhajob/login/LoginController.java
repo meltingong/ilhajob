84d2d8dd837dbc82a7cbc459ad17aaf075909c86
chosd <racoCSD@gmail.com>
2023-04-05 18:24:36 +0900

    login controller 시작

--- src/main/java/com/itwill/ilhajob/login/LoginController.java
+++ src/main/java/com/itwill/ilhajob/login/LoginController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 	
-//	@RequestMapping(value =  {"/user_login_action","/corp_login_action"})
-//	public String login_action() {
-//		
-//		return "";
-//	}
 	@RequestMapping(value =  "login_action")
 	public String login_action() {
 		
