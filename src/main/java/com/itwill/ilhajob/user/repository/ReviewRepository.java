5e752206328a19473ce3f102e13ec61870fafed7
minyoung.c <devcmy136@gmail.com>
2023-04-12 00:39:46 +0900

    Service갈아엎기전, ReviewRepository 작성중

--- src/main/java/com/itwill/ilhajob/user/repository/ReviewRepository.java
+++ src/main/java/com/itwill/ilhajob/user/repository/ReviewRepository.java
@@ -1,9 +1,30 @@
 package com.itwill.ilhajob.user.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.itwill.ilhajob.user.dto.UserDto;
 import com.itwill.ilhajob.user.entity.Review;
+import com.itwill.ilhajob.user.entity.User;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
+	//findBy+entity의 컬럼이름 으로 찾아줌
+	List<Review> findByUser(String userEmail);
+	//Optional이 필요한경우
+	//Optional<List<Review>> findByUser(String userEmail);
+	
+	List<Review> findByCorp(String corpId);
+	//Optional<List<Review>> findByCorp(String corpId);
+ }
+/* <<ServiceImpl에서 작성이 필요한 부분>>
+ * Optional<List<Person>> optionalPersons = personRepository.findByFirstName("John");
+ 
+if (optionalPersons.isPresent()) {
+List<Person> persons = optionalPersons.get();
+// 결과를 처리하는 코드 작성
+} else {
+// 결과가 없는 경우 처리하는 코드 작성
 }
+*/
