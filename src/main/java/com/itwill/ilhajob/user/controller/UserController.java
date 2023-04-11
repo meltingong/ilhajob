a89934ef8b2caa7f40a9e326a3f763232fa822c9
minyoung.c_itwill <devcmy136@gmail.com>
2023-04-11 17:07:02 +0900

    **

--- src/main/java/com/itwill/ilhajob/user/controller/UserController.java
+++ src/main/java/com/itwill/ilhajob/user/controller/UserController.java
@@ -184,6 +184,7 @@ public class UserController {
 		//request.getSession().setAttribute("sUserId", "test3@test.com");
 		String sUserId = (String)request.getSession().getAttribute("sUserId");
 		UserDto loginUser = userService.findUser(sUserId);
+		
 		System.out.println(loginUser);
  		request.setAttribute("loginUser", loginUser);
 		forwardPath = "/candidate-dashboard-applied-job";
