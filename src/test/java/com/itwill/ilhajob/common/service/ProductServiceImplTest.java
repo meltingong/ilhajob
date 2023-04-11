8266128bb4f1bcb914e388f93d376c145dcfe408
ys9890lggdd@naver.com <ys9890lggdd@naver.com>
2023-04-11 18:42:28 +0900

    ..

---
+++ src/test/java/com/itwill/ilhajob/common/service/ProductServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.itwill.ilhajob.common.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.itwill.ilhajob.FinalProjectTeam1IlhajobApplicationTests;
+
+class ProductServiceImplTest extends FinalProjectTeam1IlhajobApplicationTests {
+    @Autowired
+    ProductService productService;
+	
+    @Disabled
+	@Test
+	void testselectByNo() throws Exception {
+    	System.out.println(productService.selectByNo(1));	
+	}
+
+}
