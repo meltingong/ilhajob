5645534f98ec64753430d6b247c38cea53d621c4
meltingong <hwk1210@naver.com>
2023-04-12 12:14:06 +0900

    messageService 작성중

--- src/main/java/com/itwill/ilhajob/user/service/MessageServiceImpl.java
+++ src/main/java/com/itwill/ilhajob/user/service/MessageServiceImpl.java
@@ -1,5 +1,51 @@
 package com.itwill.ilhajob.user.service;
 
-public class MessageServiceImpl {
+import java.util.List;
+
+import com.itwill.ilhajob.user.dto.MessageDto;
+
+public class MessageServiceImpl implements MessageService{
+
+	@Override
+	public int createMessage(MessageDto message) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateMessage(MessageDto message) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int removeMessageBySeq(Long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int removeMessageByUserSeq(Long userId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public MessageDto findMessageDetail(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MessageDto> findMessageList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MessageDto> fineMessageOfUser(Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
