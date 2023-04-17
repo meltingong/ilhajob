78b694b16ca7b0428bb380b555eb27595f42d6b0
21Hour <leepoii0804@gmail.com>
2023-04-17 17:32:55 +0900

    ..

--- src/main/java/com/itwill/ilhajob/common/controller/ModalController.java
+++ src/main/java/com/itwill/ilhajob/common/controller/ModalController.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -16,17 +15,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itwill.ilhajob.common.dto.ProductDto;
-import com.itwill.ilhajob.common.entity.Product;
 import com.itwill.ilhajob.common.service.ProductService;
 
 @RestController
