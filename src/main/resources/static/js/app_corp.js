922c58b7c409c752e1c2612a2cb2ba50e05058e8
kangcarry <111220895+kangcarry@users.noreply.github.com>
2023-04-18 22:46:15 +0900

    corptag (rest ajax방식요청) 작업중

--- src/main/resources/static/js/app_corp.js
+++ src/main/resources/static/js/app_corp.js
@@ -1,29 +1,23 @@
 import * as View from "./view.js";
 import * as Request from "./request.js";
 
-$('#changeTag').click(function(){
+$('.changeTag').click(function(e){
 	console.log("펑션접근");
 	/*
 		url,method,contentType,sendData,function,async
 	*/
 	
 		let sendData={
-			tagId=$('#changeTag').value()
+			tagId:$(this).val()
 		};
 	console.log(sendData);
-	Request.ajaxRequest('/getTagData',
-					    'GET',
+	Request.ajaxRequest('getTagData',
+					    'POST',
 						'application/json;charset=UTF-8',
 						JSON.stringify(sendData),
-						console.log('컨트롤러보내기직전'),
 						function(resultJson){
-							if(resultJson.data!=null){
-								console.log('요청 URL 성공');
-								//ajaxRequest();
+							console.log("제이슨 받아왔다");
 								View.render("#tag-filter-template",resultJson,"#corp-main-list");
-							}else{
-								alert(resultJson);
-							}
 						},
 						true); //비동기
 	e.preventDefault();
