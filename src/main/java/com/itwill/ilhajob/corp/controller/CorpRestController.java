1edc9b902bb17cad116fa5abdae39899e50ac0d4
songdh97 <songdh97@gmail.com>
2023-04-18 18:11:50 +0900

    search ajax 진행중ㅠㅠ

--- src/main/java/com/itwill/ilhajob/corp/controller/CorpRestController.java
+++ src/main/java/com/itwill/ilhajob/corp/controller/CorpRestController.java
@@ -10,11 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.itwill.ilhajob.common.dto.CorpTagDto;
 import com.itwill.ilhajob.common.service.CorpTagService;
+import com.itwill.ilhajob.corp.dto.CorpDto;
+import com.itwill.ilhajob.corp.service.CorpService;
 
 @RestController
 public class CorpRestController {
 	@Autowired
 	private CorpTagService corpTagService;
+	@Autowired
+	private CorpService corpService;
 	
 	@GetMapping("/getTagData")
 	public HashMap<String, Object> getTagData(@RequestParam Long tagId){
@@ -24,4 +28,13 @@ public class CorpRestController {
 		System.out.println(corpTagList);
 		return map;
 	}
+	
+	//corpName으로 검색
+	@GetMapping("/search")
+	public List<CorpDto> searchByCorpName(@RequestParam("corpName")String corpName) throws Exception{
+		List<CorpDto> corpSearchList=corpService.searchByCorpName(corpName);
+		return corpSearchList;
+	}
+	
+	
 }
