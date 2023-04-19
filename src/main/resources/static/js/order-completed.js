667194e048b76f15a6b284579833d7a0799b5b9c
21Hour <leepoii0804@gmail.com>
2023-04-19 10:57:03 +0900

    order-completed완료

--- src/main/resources/static/js/order-completed.js
+++ src/main/resources/static/js/order-completed.js
@@ -3,19 +3,23 @@
 	$(document).ready(function() {
 		// localStorage에서 데이터를 불러오기
 		const data = JSON.parse(localStorage.getItem("data"));
-
 		// 데이터를 사용하여 DOM을 변경
-		$("#order-id").text(data);
-		$("#order-date").text(data.age);
-		$("#order-total").text(data.age);
-		$(".product-name").text(data.age);
-		$(".product-total").text(data.age);
-		$(".order-period").text(data.age);
-		$(".amount").text(data.age);
-
-		// localStorage 데이터를 삭제
-		localStorage.removeItem("data");
+		const productPrice = parseInt(data.productData.productPrice);
+		
+		$(".order-info #order-id").text(data.orderData.id);
+		$(".order-info #order-date").text(data.orderData.orderStartDate);
+		$(".order-info #order-total").text(productPrice.toLocaleString+"원");
+		$(".order-info #order-method").text(data.paymentData.paymentMethod);
+		$(".product-name").text(data.productData.productName);
+		$(".product-total").text(productPrice.toLocaleString+"원");
+		$(".order-period").text(data.orderData.orderEndDate);
+		$(".amount").text(data.productData.orderTotal);
+		
 	});
 
+	$(window).on('beforeunload', function() {
+		// 페이지 전환시 localStorage 데이터를 삭제
+		localStorage.removeItem("data");localStorage.removeItem("data");
+	});
 
 })(window.jQuery);
