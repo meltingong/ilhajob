ed52eaf0dfb1ba9b873690adfd7de4786de6c769
chosd <racoCSD@gmail.com>
2023-04-20 20:28:45 +0900

    ..

--- src/main/java/com/itwill/ilhajob/corp/controller/CorpImageController.java
+++ src/main/java/com/itwill/ilhajob/corp/controller/CorpImageController.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -25,7 +26,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.itwill.ilhajob.common.controller.ResponseStatusCode;
-import com.itwill.ilhajob.common.utils.ResourceRefresher;
 import com.itwill.ilhajob.corp.dto.CorpDto;
 import com.itwill.ilhajob.corp.dto.CorpImageDto;
 import com.itwill.ilhajob.corp.service.CorpImageService;
@@ -42,10 +42,10 @@ public class CorpImageController {
 	private ResourceLoader resourceLoader;
 	
 	
-	// 여러이미지 업로드
+	// 기업로고 업로드
 	@ResponseBody
-	@PostMapping(value = "/image-upload-action")
-	public ResponseEntity<Object> image_upload_action(@RequestParam("images") List<MultipartFile> images, HttpServletRequest request) throws Exception {
+	@PostMapping(value = "/corp-logo-upload-action")
+	public String corp_logo_upload_action(@RequestParam("images") List<MultipartFile> images, HttpServletRequest request) throws Exception {
 		request.getSession().setAttribute("sCorpId", 1L); //임시로 아이디 로그인상태
 		Long sCorpId =(Long)request.getSession().getAttribute("sCorpId");
 		CorpDto corp=corpService.findByCorpId(sCorpId);
@@ -56,7 +56,7 @@ public class CorpImageController {
 		absPath.add("logo/");
 		
 		String urlPath="";
-		String absolutePath="c://final-project-team1-ilhajob/";
+		String absolutePath="c:/final-project-team1-ilhajob/";
 		for (String string : absPath) {
 			absolutePath+=string;
 			urlPath+=string;
@@ -76,18 +76,43 @@ public class CorpImageController {
 	            // 파일 저장 로직
 	            try {
 	                byte[] bytes = image.getBytes();
-	                Path path = Paths.get(absolutePath + fileName);
+	                Path path = Paths.get(absolutePath + saveFileName);
 	                Files.write(path, bytes);
-	                corp.setCorpOriginalFileName(fileName);
-	                corp.setCorpStoredFileName(urlPath + fileName);
+	                corp.setCorpOriginalFileName(saveFileName);
+	                corp.setCorpStoredFileName(urlPath + saveFileName);
 	                corpService.update(corp.getId(), corp);
-	                ResourceRefresher.refreshResources("classpath:/upload/logo/**");
-	                return ResponseEntity.ok().body("{\"success\": true, \"imagePath\": \"" + corp.getCorpStoredFileName() + "\", \"message\": \"리뷰가 성공적으로 작성되었습니다.\"}");
+	                //return ResponseEntity.ok().body("{\"success\": true, \"imagePath\": \"" + corp.getCorpStoredFileName() + "\", \"message\": \"리뷰가 성공적으로 작성되었습니다.\"}");
+	                return "이미지 업로드 완료";
 	            } catch (IOException e) {
-	            	return ResponseEntity.status(5101).body("{\"success\": false, \"message\": \"이미지 업로드 실패.\"}");
+	            	//return ResponseEntity.status(5101).body("{\"success\": false, \"message\": \"이미지 업로드 실패.\"}");
+	            	return "이미지 업로드 실패...";
 	            }
 	        }
 	    }
 	    return null;
 	}
+	
+	public Map<String, String> makeDir(String path){
+		//절대경로 저장
+		List<String> absPath = new ArrayList<String>();
+		absPath.add("upload/");
+		absPath.add(path+"/");
+		
+		String urlPath="";
+		String absolutePath="c:/final-project-team1-ilhajob/";
+		
+		for (String string : absPath) {
+			absolutePath+=string;
+			urlPath+=string;
+			File folder = new File(absolutePath);
+			if(!folder.exists()) {
+				folder.mkdir();
+				System.out.println(folder+"폴더가 생성되었습니다");
+			}else {
+				System.out.println(folder+"이미 폴더가 존재합니다");
+			}
+		}
+		return null;
+	}
+
 }
--- src/main/resources/static/js/image-upload.js
+++ src/main/resources/static/js/image-upload.js
@@ -8,6 +8,21 @@ import *  as View from "./view.js";
 
 //$('#init-page').trigger('click');
 //====================================================
+var uploadButtonCorp = {
+        $button: $('.uploadButton-input-corp'),
+        $nameField: $('.uploadButton-file-name-corp')
+    };
+uploadButtonCorp.$button.on('change', function() {
+    _populateFileField($(this));
+});
+
+function _populateFileField($button) {
+    var selectedFile = [];
+    for (var i = 0; i < $button.get(0).files.length; ++i) {
+        selectedFile.push($button.get(0).files[i].name + '<br>');
+    }
+    uploadButtonCorp.$nameField.html(selectedFile);
+}
 
 $('#upload').change(function(){
 	let input = this;
@@ -20,8 +35,20 @@ $('#upload').change(function(){
 		reader.readAsDataURL(input.files[0]);
 	}
 });
-//이미지 여러개
-$('#upload-btn').click(function() {
+
+$('#corp-image-upload').change(function(){
+	let input = this;
+	if (input.files && input.files[0]) {
+		let reader = new FileReader();
+		reader.onload = function(e) {
+			$('#corp-image-preview')
+				.attr('src', e.target.result);
+		};
+		reader.readAsDataURL(input.files[0]);
+	}
+});
+//기업 로고 이미지 업로드
+$('#corp-logo-upload-btn').click(function() {
 	let formData = new FormData();
 	let files = $('input[name="images"]').get(0).files;
 	if (files.length > 0) {
@@ -29,7 +56,7 @@ $('#upload-btn').click(function() {
 			formData.append("images", files[i]);
 		}
 		$.ajax({
-			url: "image-upload-action",
+			url: "corp-logo-upload-action",
 			type: "POST",
 			data: formData,
 			async: false,
@@ -48,5 +75,33 @@ $('#upload-btn').click(function() {
 		alert("No images selected.");
 	}
 });
-	
+
+//기업 이미지 여러장 업로드
+$('#corp-logo-upload-btn').click(function() {
+	let formData = new FormData();
+	let files = $('input[name="corp-images"]').get(0).files;
+	if (files.length > 0) {
+		for (let i = 0; i < files.length; i++) {
+			formData.append("corp-images", files[i]);
+		}
+		$.ajax({
+			url: "corp-image-upload-action",
+			type: "POST",
+			data: formData,
+			async: false,
+			processData: false,
+			contentType: false,
+			success: function(data) {
+				alert(data);
+				window.location.href="/final-project-team1-ilhajob/dashboard-company-profile"
+				//View.render("#image-upload-form",data,"#image-content");
+			},
+			error: function() {
+				alert("이미지 업로드 error");
+			}
+		});
+	} else {
+		alert("No images selected.");
+	}
+});
 
