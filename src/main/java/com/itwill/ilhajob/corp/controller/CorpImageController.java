8a41c9cef64c9eab63a6d12da5d149030e103f9c
chosd <racoCSD@gmail.com>
2023-04-20 18:04:06 +0900

    ..

--- src/main/java/com/itwill/ilhajob/corp/controller/CorpImageController.java
+++ src/main/java/com/itwill/ilhajob/corp/controller/CorpImageController.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.itwill.ilhajob.common.controller.ResponseStatusCode;
+import com.itwill.ilhajob.common.utils.ResourceRefresher;
 import com.itwill.ilhajob.corp.dto.CorpDto;
 import com.itwill.ilhajob.corp.dto.CorpImageDto;
 import com.itwill.ilhajob.corp.service.CorpImageService;
@@ -53,7 +54,6 @@ public class CorpImageController {
 		List<String> absPath = new ArrayList<String>();
 		absPath.add("upload/");
 		absPath.add("logo/");
-		absPath.add(corp.getCorpName()+"/");
 		
 		String absolutePath="c://final-project-team1-ilhajob/";
 		for (String string : absPath) {
@@ -69,13 +69,13 @@ public class CorpImageController {
 		
 		//상대경로 저장
 		String staticPath="";
-		String relativePath="src/main/resources/static/";
+		String relativePath="src/main/resources/";
 		for (String string : absPath) {
 			relativePath+=string;
 			staticPath+=string;
 			File folder = new File(relativePath);
 			if(!folder.exists()) {
-				folder.mkdir();
+				//folder.mkdir();
 				System.out.println(folder+"폴더가 생성되었습니다");
 			}else {
 				System.out.println(folder+"이미 폴더가 존재합니다");
@@ -85,14 +85,14 @@ public class CorpImageController {
 		// MultipartFile 배열로 받은 파일을 처리하는 로직
 	    for (MultipartFile image : images) {
 	        if (!image.isEmpty()) {
-	            String fileName = image.getOriginalFilename();
+	            String fileName = corp.getId()+"_"+image.getOriginalFilename();
 	            // 파일 저장 로직
 	            try {
 	                byte[] bytes = image.getBytes();
 	                Path path = Paths.get(absolutePath + fileName);
 	                Files.write(path, bytes);
-	                Path rePath = Paths.get(relativePath + fileName);
-	                Files.write(rePath, bytes);
+	                //Path rePath = Paths.get(relativePath + fileName);
+	                //Files.write(rePath, bytes);
 	                //corpImage create
 //	                CorpImageDto corpImageDto = CorpImageDto.builder()
 //	                										.originalFileName(fileName)
@@ -103,6 +103,7 @@ public class CorpImageController {
 	                corp.setCorpOriginalFileName(fileName);
 	                corp.setCorpStoredFileName(staticPath + fileName);
 	                corpService.update(corp.getId(), corp);
+	                ResourceRefresher.refreshResources("classpath:/upload/logo/**");
 	                return ResponseEntity.ok().body("{\"success\": true, \"imagePath\": \"" + corp.getCorpStoredFileName() + "\", \"message\": \"리뷰가 성공적으로 작성되었습니다.\"}");
 	            } catch (IOException e) {
 	            	return ResponseEntity.status(5101).body("{\"success\": false, \"message\": \"이미지 업로드 실패.\"}");
