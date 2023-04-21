2b99a05bbf31ef42781bd91796d8a504b2fc6a21
ys9890lggdd@naver.com <ys9890lggdd@naver.com>
2023-04-21 18:38:28 +0900

    Main Footer 수정

---
+++ src/main/java/com/itwill/ilhajob/common/controller/ProductController.java
@@ -0,0 +1,41 @@
+package com.itwill.ilhajob.common.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.itwill.ilhajob.common.dto.ProductDto;
+import com.itwill.ilhajob.common.service.OrdersService;
+import com.itwill.ilhajob.common.service.ProductService;
+
+import groovyjarjarantlr4.v4.parse.ANTLRParser.finallyClause_return;
+import net.bytebuddy.asm.Advice.OffsetMapping.Sort;
+
+@Controller
+public class ProductController {
+	
+	private final ProductService productService;
+	
+	@Autowired
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+	
+	@GetMapping("/shop")
+	public String shop(HttpServletRequest request, Model model) throws Exception {
+		String forwardPath = "";
+		String pDiv=(String)request.getSession().getAttribute("pDiv");
+		List<ProductDto> productList = productService.selectByDiv(pDiv);
+		model.addAttribute("productList", productList);
+		
+	    forwardPath = "shop";
+	    return forwardPath;
+	}
+		
+}
