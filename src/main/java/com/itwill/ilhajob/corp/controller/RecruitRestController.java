fd69a2683b09272a51aa0aacf2a6ec718402933f
kangcarry <111220895+kangcarry@users.noreply.github.com>
2023-04-23 00:50:22 +0900

    corp,recruit list html 수정

--- src/main/java/com/itwill/ilhajob/corp/controller/RecruitRestController.java
+++ src/main/java/com/itwill/ilhajob/corp/controller/RecruitRestController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +21,10 @@ import com.itwill.ilhajob.common.service.RecruitTagService;
 import com.itwill.ilhajob.common.service.TagService;
 import com.itwill.ilhajob.corp.dto.RecruitDto;
 import com.itwill.ilhajob.corp.service.RecruitService;
+import com.itwill.ilhajob.user.dto.RecruitScrapDto;
+import com.itwill.ilhajob.user.dto.UserDto;
+import com.itwill.ilhajob.user.service.RecruitScrapService;
+import com.itwill.ilhajob.user.service.UserService;
 
 @RestController
 public class RecruitRestController {
@@ -28,15 +34,47 @@ public class RecruitRestController {
 	private TagService tagService;
 	@Autowired
 	private RecruitTagService recruitTagService;
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private RecruitScrapService recruitScrapService;
 	
 	@PostMapping(value="/getRecruitTag", produces = "application/json;charset=UTF-8")
-	public Map<String,Object> getRecruitTagData(@RequestBody Map<String,String> data) throws Exception{
+	public Map<String,Object> getRecruitTagData(@RequestBody Map<String,String> data,HttpServletRequest request) throws Exception{
 		System.out.println("컨트롤러도착");
 		Map<String, Object> map = new HashMap<String,Object>();
 		List<RecruitDto> recruitList = recruitService.findRecruitAll();
 		List<RecruitTagListDto> recruitTagListDto = new ArrayList<RecruitTagListDto>();
 		Long id = 0L;
 		List<TagDto> tagList = new ArrayList<TagDto>();
+		
+		String sUserId = (String)request.getSession().getAttribute("sUserId");
+		UserDto loginUser = userService.findUser(sUserId);
+		System.out.print("아이디:"+loginUser.getId());
+		map.put("loginUser", loginUser);
+		
+		//스크랩 (북마크)로고 출력
+			List<RecruitScrapDto> recruitScrapList = recruitScrapService.sellectByUserId(loginUser.getId());
+			//스크랩확인 카운트 리스트
+			List<Integer> countList = new ArrayList<Integer>();
+			countList.add(0); //테이블은 1부터 id가 있으니 처음은 0
+			
+			for(RecruitDto recruit :recruitList) {
+			    boolean hasRecruitScrap = false; // 리크루트 스크랩이 있는지 여부를 나타내는 변수
+			    for(RecruitScrapDto recruitscrap:recruitScrapList) {
+			        if(recruit.getId() == recruitscrap.getRecruit().getId()) {
+			            hasRecruitScrap = true; // 리크루트 스크랩이 있을 때
+			            break; // 리크루트 스크랩이 있으면 반복문 종료
+			        }
+			    }
+			    if(hasRecruitScrap) {
+			        countList.add(1); // 리크루트 스크랩이 있을 때
+			    } else {
+			        countList.add(0); // 리크루트 스크랩이 없을 때
+			    }
+			}
+			System.out.println("로그인유저:"+loginUser+"카운트"+countList+"스크랩리스트"+recruitScrapList);
+			map.put("countList", countList);
 		//전체태그선택
 		if(data.get("tagId").equals("전체")) {
 			List<RecruitTagDto> recruitTagList = recruitTagService.selectAll();
@@ -73,8 +111,11 @@ public class RecruitRestController {
 		
 		map.put("data", recruitTagListDto);
 		System.out.println("태그선택실행완료");
+		System.out.println(map);
 		return map;
 		
 		}
 	}
 }
+
+	
