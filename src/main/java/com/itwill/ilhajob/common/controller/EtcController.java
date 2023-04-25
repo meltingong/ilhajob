40fe38b20e81be97634e288328b623875f0ca10c
breeeeeeeeze <earth9563@gmail.com>
2023-04-25 21:03:37 +0900

    index에서 마감 임박 공고 노출

--- src/main/java/com/itwill/ilhajob/common/controller/EtcController.java
+++ src/main/java/com/itwill/ilhajob/common/controller/EtcController.java
@@ -1,4 +1,4 @@
-package com.itwill.ilhajob.corp.controller;
+package com.itwill.ilhajob.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
