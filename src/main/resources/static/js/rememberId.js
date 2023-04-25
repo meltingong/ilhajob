011b41c704ef2112f85feccd3a9fe2e9d425a053
meltingong <hwk1210@naver.com>
2023-04-25 16:02:21 +0900

    로그인 remember 수정중....

--- src/main/resources/static/js/rememberId.js
+++ src/main/resources/static/js/rememberId.js
@@ -1,22 +1,50 @@
-// 'remember me' 체크박스가 변경될 때마다, localStorage에 값을 저장 또는 삭제합니다.
-$(document).on('change', '#remember', function() {
-  let emailInput = document.getElementById('email');
-  let emailValue = emailInput.value.trim();
-
-  if (this.checked) {
-    localStorage.setItem('loginEmail', emailValue);
-  } else {
-    localStorage.removeItem('loginEmail');
-  }
-});
-
-// 페이지가 로드될 때, localStorage에 저장된 값을 확인하여, 로그인 아이디를 채웁니다.
 $(document).ready(function() {
-  let emailInput = document.getElementById('email');
-  let loginEmail = localStorage.getItem('loginEmail');
+  // Get saved data from localStorage
+  var email;
+  var remember = localStorage.getItem('remember');
 
-  if (loginEmail !== null) {
-    emailInput.value = loginEmail;
-    document.getElementById('remember').checked = true;
+  if ($('#candidate-btn').hasClass('btn-style-seven')) {
+    email = localStorage.getItem('email');
+  } else if ($('#corp-btn').hasClass('btn-style-seven')) {
+    email = localStorage.getItem('id');
   }
+  console.log(email);
+  // Check if remember checkbox was checked previously
+  if (remember == 'true' && $('#candidate-btn').hasClass('btn-style-seven')) {
+    // Set email and password fields to saved values
+    $('#email').val(email);
+    $('#remember').prop('checked', true);
+  } else if (remember == 'true' && $('#corp-btn').hasClass('btn-style-seven')) {
+    $('#id').val(email);
+    $('#remember').prop('checked', true);
+  }
+
+  // Save email and password to localStorage on login
+  $('#log-in').click(function() {
+    var email;
+    var remember = $('#remember').is(':checked');
+
+    if ($('#candidate-btn').hasClass('btn-style-seven')) {
+      // Set email and password fields to saved values
+      email = $('#email').val();
+      localStorage.setItem('email', email);
+    } else if ($('#corp-btn').hasClass('btn-style-seven')) {
+      email = $('#id').val();
+      localStorage.setItem('id', email);
+    }
+
+    localStorage.setItem('remember', remember);
+  });
+
+  // Clear saved data from localStorage on logout
+	$('#logout-btn').click(function() {
+	  var remember = localStorage.getItem('remember');
+	  
+	  if ($('#candidate-btn').hasClass('btn-style-seven') && remember == 'false') {
+	    localStorage.removeItem('email');
+	  } else if ($('#corp-btn').hasClass('btn-style-seven') && remember == 'false') {
+	    localStorage.removeItem('id');
+	  }
+	  localStorage.removeItem('remember');
+});
 });
