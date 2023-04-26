55677c294e9347b2ceedc05856cd0803a809c996
meltingong <hwk1210@naver.com>
2023-04-26 13:50:52 +0900

    메시지 수정 완료

--- src/main/resources/static/js/message.js
+++ src/main/resources/static/js/message.js
@@ -1,7 +1,10 @@
 function deleteMessage(messageId){
-	console.log(">>"+messageId);
+	var input = document.createElement('input');
+    input.type = 'hidden';
+    input.name = 'messageId';
+    input.value = messageId;
+    document.getElementById('listId').appendChild(input);
 	
-	$('#messageId'+messageId).val(messageId);
 	
 	document.f.action = "alerts-remove";
 	document.method='POST';
