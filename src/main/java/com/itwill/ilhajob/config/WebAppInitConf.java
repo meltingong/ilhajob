5f57c11589978c5e5d71dce2f3f6aac41591e48d
doki0804 <doki0804@naver.com>
2023-04-26 21:21:26 +0900

    ...

--- src/main/java/com/itwill/ilhajob/config/WebAppInitConf.java
+++ src/main/java/com/itwill/ilhajob/config/WebAppInitConf.java
@@ -6,8 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
